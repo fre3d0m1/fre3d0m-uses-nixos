67f48d353032947ebda06da2e1f3b3f04866d08c
fre3d0m1 <pupperfishh@proton.me>
2026-08-25 20:28:52 -0400

    Convert hyprland animations into Nix

--- modules/home/hyprland/config/look_and_feel.nix
+++ modules/home/hyprland/config/look_and_feel.nix
@@ -1,4 +1,9 @@
 { ... }:
+let
+    makeCurve = speed: properties: {
+      _args = [ speed properties ];
+    };
+in
 {
     wayland.windowManager.hyprland.settings = {
         config = {
@@ -64,42 +69,40 @@
             force_default_wallpaper = -1;
             disable_hyprland_logo = false;
           };
+
+          animations = {
+            enabled = true;
+          };
         };
-        
-        # extraConfig = ''
-        #   animations = {
-        #     enabled = true;
-        #
-        #     # Curves (Beziers)
-        #     bezier = [
-        #       "easeOutQuint, 0.23, 1, 0.32, 1"
-        #       "easeInOutCubic, 0.65, 0.05, 0.36, 1"
-        #       "linear, 0, 0, 1, 1"
-        #       "almostLinear, 0.5, 0.5, 0.75, 1"
-        #       "quick, 0.15, 0, 0.1, 1"
-        #     ];
-        #
-        #     # Animation rules: [ "NAME, ENABLED, SPEED, CURVE [, STYLE]" ]
-        #     animation = [
-        #       "global, 1, 10, default"
-        #       "border, 1, 5.39, easeOutQuint"
-        #       "windows, 1, 4.79, default"
-        #       "windowsIn, 1, 4.1, default, popin 87%"
-        #       "windowsOut, 1, 1.49, linear, popin 87%"
-        #       "fadeIn, 1, 1.73, almostLinear"
-        #       "fadeOut, 1, 1.46, almostLinear"
-        #       "fade, 1, 3.03, quick"
-        #       "layers, 1, 3.81, easeOutQuint"
-        #       "layersIn, 1, 4, easeOutQuint, fade"
-        #       "layersOut, 1, 1.5, linear, fade"
-        #       "fadeLayersIn, 1, 1.79, almostLinear"
-        #       "fadeLayersOut, 1, 1.39, almostLinear"
-        #       "workspaces, 1, 1.94, almostLinear, fade"
-        #       "workspacesIn, 1, 1.21, almostLinear, fade"
-        #       "workspacesOut, 1, 1.94, almostLinear, fade"
-        #       "zoomFactor, 1, 7, quick"
-        #     ];
-        #   };
-        # '';
+
+        curve = [
+          (makeCurve "easeOutQuint" { type = "bezier"; points = [ [ 0.23 1 ] [ 0.32 1 ] ]; })
+          (makeCurve "easeInOutQuint" { type = "bezier"; points = [ [ 0.65 0.05 ] [ 0.36 1 ] ]; })
+          (makeCurve "linear" { type = "bezier"; points = [ [ 0 0 ] [ 1 1 ] ]; })
+          (makeCurve "almostLinear" { type = "bezier"; points = [ [ 0.5 0.5 ] [ 0.75 1 ] ]; })
+          (makeCurve "quick" { type = "bezier"; points = [ [ 0.15 0 ] [ 0.1 1 ] ]; })
+
+          (makeCurve "easy" { type = "spring"; mass = 1; stiffness = 71.2633; dampening = 15.8273644; })
+        ];
+
+        animation = [
+          { leaf = "global"; enabled = true; speed = 10; bezier = "default"; }
+          { leaf = "border"; enabled = true; speed = 5.39; bezier = "easeOutQuint"; }
+          { leaf = "windows"; enabled = true; speed = 4.79; spring = "easy"; }
+          { leaf = "windowsIn"; enabled = true; speed = 4.1; spring = "easy"; style = "popin 87%"; }
+          { leaf = "windowsOut"; enabled = true; speed = 1.49; bezier = "linear"; style = "popin 87%"; }
+          { leaf = "fadeIn"; enabled = true; speed = 1.73; bezier = "almostLinear"; }
+          { leaf = "fadeOut"; enabled = true; speed = 1.46; bezier = "almostLinear"; }
+          { leaf = "fade"; enabled = true; speed = 3.03; bezier = "quick"; }
+          { leaf = "layers"; enabled = true; speed = 3.81; bezier = "easeOutQuint"; }
+          { leaf = "layersIn"; enabled = true; speed = 4; bezier = "easeOutQuint"; style = "fade"; }
+          { leaf = "layersOut"; enabled = true; speed = 1.5; bezier = "linear"; style = "fade"; }
+          { leaf = "fadeLayersIn"; enabled = true; speed = 1.79; bezier = "almostLinear"; }
+          { leaf = "fadeLayersOut"; enabled = true; speed = 1.39; bezier = "almostLinear"; }
+          { leaf = "workspaces"; enabled = true; speed = 1.94; bezier = "almostLinear"; style = "fade"; }
+          { leaf = "workspacesIn"; enabled = true; speed = 1.21; bezier = "almostLinear"; style = "fade"; }
+          { leaf = "workspacesOut"; enabled = true; speed = 1.94; bezier = "almostLinear"; style = "fade"; }
+          { leaf = "zoomFactor"; enabled = true; speed = 7; bezier = "quick"; }
+        ];
     };
 }
